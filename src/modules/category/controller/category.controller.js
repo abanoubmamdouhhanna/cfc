806084148a303e22c398cf087a9145cb7f9f8490
abanoubmamdouhhanna <beboabanoub332001@gmail.com>
2025-04-25 15:56:03 +0300

    fadd desc

--- src/modules/category/controller/category.controller.js
+++ src/modules/category/controller/category.controller.js
@@ -83,7 +83,7 @@ export const getCategoriesWithMeals = asyncHandler(async (req, res, next) => {
       populate: {
         path: "viewMeals",
         select:
-          "title image flavor discount finalPrice finalComboPrice size wishUser status",
+         "title image discount description finalPrice finalComboPrice wishUser status",
       },
     });
 
